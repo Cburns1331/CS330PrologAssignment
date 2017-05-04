cfcd3f7b17bef24b7ff8162548edf643728bc04c
Chris Burns <burns.christopher13@gmail.com>
2017-05-04 00:02:47 -0400

    Adding in entries

---
+++ CS330.pl
@@ -0,0 +1,19 @@
+question(info, last_name, 'What is your last name?', 1).
+question(info, mom_maiden_name, 'What is your moms maiden name', 8).
+question(info, first_pet, 'What was your first pets name', 3).
+question(info, last_high_school, 'Where did you last attend High School?', 5).
+question(info, mom_birthplace, 'Where was your mom born?', 9).
+question(info, num_siblings, 'How many siblings do you have?', 2).
+
+question(usage, job_title, 'What is your job title?', 4).
+question(usage, home_directory, 'What is your home directory?', 5).
+question(usage, last_date_of_login, 'When did you last login?', 6).
+question(usage, boss_name, 'What is your boss name?', 3).
+question(usage, office_number, 'What number office do you work in?', 4).
+
+question(presence, time_of_day, 'What is the time of day?', 1).
+question(presence, season, 'What season is it?', 2).
+question(presence, weather_yesterday, 'What was the weather like yesterday?', 3).
+question(presence, month_of_last_big_storm, 'What month was the last big storm in?', 6).
+question(presence, randomly_generated_riddle, 'Randomly generated riddle here...', 8).
+question(presence, captcha, 'Prove you are not a robot by solving this captcha', 7).
